fe9ac5a37b0c1d86cfaca7e731da65dfa5f49d78
Ana Nishimoto <ana.nishimoto@epiphany-rbc.com>
2022-10-03 19:20:16 +0200

    Added duration calculation to the attribute value

--- index.js
+++ index.js
@@ -1,20 +1,56 @@
 // Stopwatch exercise
 
 function Stopwatch() {
-    
-    this.duration = 0;
+
+    const startTimer = {
+        state: false,
+        value: 0,
+    }
+
+    const stopTimer = {
+        state: false,
+        value: 0,
+    }
+
+    this.duration = '0 seconds';
     
     this.start = function() {
-        const startTime = new Date().getTime();
-        console.log('start', startTime);
+
+        if(startTimer.state) {
+            console.error('The timer is already running')
+            return;
+        } 
+
+        if(stopTimer.state) {
+            stopTimer.state = false;
+        }
+
+        startTimer.state = true;
+        startTimer.value = new Date().getTime()/1000;
     }
 
     this.stop = function() {
-        const finishTime = new Date().getTime();
-        console.log('stop', finishTime)
+        
+        if(stopTimer.state){
+            console.error('You already stopped the timer')
+            return
+        }
+
+        if(!startTimer.state){
+            console.error('You need to start the timer before stopping it')
+            return
+        }
+
+        if(startTimer.state) {
+            startTimer.state = false;
+        }
+
+        stopTimer.state = true;
+        stopTimer.value = new Date().getTime()/1000;
+        this.duration = `${Math.round(stopTimer.value - startTimer.value)} seconds`;
     }
 
     this.reset = function() {
-        this.duration = 0;
+        this.duration = '0 seconds';
     }
 }

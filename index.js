c3bbfb5aa23c074f2b5e593a200831910fab2b05
Ana Nishimoto <ana.nishimoto@epiphany-rbc.com>
2022-10-03 20:23:13 +0200

    Made the same exercise only with methods

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // Stopwatch exercise
 
+// Made with duration as an attribute
+
 function Stopwatch() {
 
     const startTimer = {
@@ -52,5 +54,65 @@ function Stopwatch() {
 
     this.reset = function() {
         this.duration = '0 seconds';
+    }
+}
+
+// Made with duration a method
+
+function Timerwatch() {
+
+    const startTimer = {
+        state: false,
+        value: 0,
+    }
+
+    const stopTimer = {
+        state: false,
+        value: 0,
+    }
+
+    this.getDuration = function() {
+        return `${Math.round(stopTimer.value - startTimer.value)} seconds`
+    }
+    
+    this.start = function() {
+
+        if(startTimer.state) {
+            console.error('The timer is already running')
+            return;
+        } 
+
+        if(stopTimer.state) {
+            stopTimer.state = false;
+        }
+
+        startTimer.state = true;
+        startTimer.value = new Date().getTime()/1000;
+    }
+
+    this.stop = function() {
+        
+        if(stopTimer.state){
+            console.error('You already stopped the timer')
+            return
+        }
+
+        if(!startTimer.state){
+            console.error('You need to start the timer before stopping it')
+            return
+        }
+
+        if(startTimer.state) {
+            startTimer.state = false;
+        }
+
+        stopTimer.state = true;
+        stopTimer.value = new Date().getTime()/1000;
+    }
+
+    this.reset = function() {
+        startTimer.value = 0;
+        stopTimer.value = 0;
+        this.duration;
     }
 }
